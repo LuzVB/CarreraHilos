91d22565e18a7c6a69fcc246488da7a1c1245ca1
Valentina <valentinaing75@gmail.com>
2020-03-17 20:46:16 -0500

    Comunicación entre  hilos

--- carreraHilos/src/main/java/edu/unicundi/carrerahilos/Corredor.java
+++ carreraHilos/src/main/java/edu/unicundi/carrerahilos/Corredor.java
@@ -49,29 +49,26 @@ public class Corredor extends Thread {
 
     private void empezarACorrer() {
         ArrayList<String> pistaUbicacion = new ArrayList<>();
-        int random, posicionActual, restaPosicion;
+        int random, posicionActual, restaPosicion, posicionFinal ;
         if (identificador == 1) {
-
-            while (equipo.getPista().get(33) != nombre) {
+            while (equipo.getPista().get(32) != nombre) {
 
                 pistaUbicacion = equipo.getPista();
                 random = (int) (Math.random() * 3) + 1;
                 posicionActual = pistaUbicacion.indexOf(nombre);
-                restaPosicion = 33-posicionActual;
+                restaPosicion = 32 - posicionActual;
                 if (restaPosicion > 3) {
-                   random = random;
-                }
-                else if(restaPosicion == 1){
+                    random = random;
+                } else if (restaPosicion == 1) {
                     if (random > 1) {
                         random = 1;
                     }
-                }
-                else if(restaPosicion == 2){
+                } else if (restaPosicion == 2) {
                     if (random > 2) {
                         random = 1;
                     }
                 }
-                
+
                 pistaUbicacion.set(posicionActual, "-");
                 pistaUbicacion.set(posicionActual + random, nombre);
                 equipo.setPista(pistaUbicacion);
@@ -82,9 +79,104 @@ public class Corredor extends Thread {
                     Logger.getLogger(Corredor.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
+
+            synchronized (equipo.getPista().get(66)) {
+                equipo.getPista().get(66).notifyAll();
+            }
+
         } else if (identificador == 2) {
+            synchronized (equipo.getPista().get(66)) {
+                try {
+                    equipo.getPista().get(66).wait();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Corredor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+       
+            while (equipo.getPista().get(65) != nombre) {
+
+                pistaUbicacion = equipo.getPista();
+                random = (int) (Math.random() * 3) + 1;
+                posicionActual = pistaUbicacion.indexOf(nombre);
+                restaPosicion = 65 - posicionActual;
+                if (restaPosicion > 3) {
+                    random = random;
+                } else if (restaPosicion == 1) {
+                    if (random > 1) {
+                        random = 1;
+                    }
+                } else if (restaPosicion == 2) {
+                    if (random > 2) {
+                        random = 1;
+                    }
+                }
+                
+                posicionFinal = posicionActual + random;
+                
+                pistaUbicacion.set(posicionActual, "-");
+                pistaUbicacion.set(posicionFinal, nombre);
+                equipo.setPista(pistaUbicacion);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Corredor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            synchronized (equipo.getPista().get(101)) {
+                equipo.getPista().get(101).notify();
+            }
+
 
         } else if (identificador == 3) {
+            synchronized (equipo.getPista().get(101)) {
+                try {
+                    equipo.getPista().get(101).wait();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Corredor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+           
+            while (equipo.getPista().get(101) != nombre) {
+//                System.out.println(equipo.getPista().size() + equipo.getNombreEquipo());
+                pistaUbicacion = equipo.getPista();
+                random = (int) (Math.random() * 3) + 1;
+                posicionActual = pistaUbicacion.indexOf(nombre);
+                restaPosicion = 101 - posicionActual;
+                if (restaPosicion > 3) {
+                    random = random;
+                } else if (restaPosicion == 1) {
+                    if (random > 1) {
+                        random = 1;
+                    }
+                } else if (restaPosicion == 2) {
+                    if (random > 2) {
+                        random = 1;
+                    }
+                }
+                
+                posicionFinal = posicionActual + random;
+                
+                if(posicionFinal >= 102){
+                    posicionFinal = 101;
+                }else if(posicionFinal == 100){
+                    posicionFinal = 101;
+                }
+                else{
+                    posicionFinal = posicionFinal;
+                }
+                
+                pistaUbicacion.set(posicionActual, "-");
+                pistaUbicacion.set(posicionFinal, nombre);
+                equipo.setPista(pistaUbicacion);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Corredor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
 
         }
     }
